e03598bb9a7bbcc013f3d046660a4c9c24d9b6be
bhargav <bhargavpatel.info@gmail.com>
2024-03-02 11:56:15 +0530

    file upload

---
+++ backendProject/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("dhavaldodiya");
+});
+
+app.get("/login", (req, res) => {
+  res.send("dhavaldodiya");
+});
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
